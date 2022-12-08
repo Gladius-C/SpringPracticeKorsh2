32a39882e9baacf53fcc89e4367fa14507997dca
Gladius-C <isip_v.n.korshunov@mpt.ru>
2022-12-08 16:24:31 +0300

    practice6spring

---
+++ src/main/java/com/example/begin/controllers/RegistrationController.java
@@ -0,0 +1,56 @@
+package com.example.begin.controllers.Repository;
+
+import com.example.begin.controllers.Repository.UserRepository;
+import com.example.begin.controllers.models.Role;
+import com.example.begin.controllers.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.Collections;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+
+    public RegistrationController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/registration")
+    private String regView(User user) {
+        return "regis";
+    }
+
+    @PostMapping("/registration")
+    private String reg(@Valid User user, BindingResult bindingResult, Model model) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+        if(userFromDb != null) {
+            model.addAttribute("message", "Пользователь с таким логином уже существует");
+            return "/regis";
+        }
+        if(user.getUsername().equals("Nigger"))
+            bindingResult.addError(new FieldError("user", "name",
+                    "Недопустимое выражение!"));
+
+        if(bindingResult.hasErrors())
+            return"/regis";
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+}
